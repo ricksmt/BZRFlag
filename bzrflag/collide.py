6f66b9ba2886b5babac0ae8e0fa24d5202032b5a
Jared Forsyth <jared@sage.aml.cs.byu.edu>
2010-03-12 11:21:19 -0700

    adding point2polygon

--- bzrflag/collide.py
+++ bzrflag/collide.py
@@ -280,6 +280,29 @@ def _rect2pts(rect):
     (x,y,w,h) = rect
     return (x,y),(x,y+h),(x+w,y+h),(x+w,y)
 
+def point_inside_polygon(self, x, y, poly):
+    """Determines if a point is inside a given polygon or not.
+
+    Polygon is a list of (x,y) pairs.
+    """
+    n = len(poly)
+    inside = False
+
+    p1x, p1y = poly[0]
+    for i in range(n + 1):
+        p2x, p2y = poly[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (y - p1y) * (p2x - p1x) \
+                            / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
 def poly2line(pts,(p1,p2)):
     '''collide a polygon with a line
     *this is tested by other methods*'''
