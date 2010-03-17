a0e4105e7775c77bb5fc594f14c2365759fa24db
Matthew Gardner <mjg82@genius.aml.cs.byu.edu>
2010-03-17 15:11:31 -0600

    Renamed the agents to make more sense

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
-./bin/bzrflag --world=maps/four_ls.bzw --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
+./bin/bzrflag --world=maps/four_ls.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 5
-./better.py localhost 50100 &
-./better.py localhost 50101 &
-./better.py localhost 50102 &
-./better.py localhost 50103 &
+./shoot_and_capture_agent.py localhost 50100 &
+./shoot_and_capture_agent.py localhost 50101 &
+./shoot_and_capture_agent.py localhost 50102 &
+./shoot_and_capture_agent.py localhost 50103 &
 
