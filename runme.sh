88cbc57ddcc50dfd35a47bc1ae9d0eb4d13f4ba8
Jared Forsyth <jared@sage.aml.cs.byu.edu>
2010-03-17 11:38:53 -0600

    add better to runme

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 ./bin/bzrflag --world=maps/simple.bzw --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 5
-./sync.py localhost 50100 &
+./better.py localhost 50100 &
 ./sync.py localhost 50101 &
-./sync.py localhost 50102 &
+./better.py localhost 50102 &
 ./sync.py localhost 50103 &
 
