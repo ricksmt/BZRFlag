62a6080e3275e4b03bee5de95ef27a99781da1e6
Matthew Gardner <mjg82@genius.aml.cs.byu.edu>
2010-03-17 12:47:51 -0600

    Added a font file (which may want to be changed). In order to add the font, I refactored a bit so we only had to specify the location in one place.

---
+++ bzrflag/paths.py
@@ -0,0 +1,11 @@
+
+import os
+
+GROUND = 'std_ground.png'
+WALL = 'wall.png'
+FONT = 'LiberationSans-Regular.ttf'
+DATA_DIR = os.path.abspath(os.path.join(
+        os.path.split(__file__)[0], '..', 'data'))
+FONT_FILE = os.path.join(DATA_DIR, FONT)
+
+# vim: et sw=4 sts=4
