3f01b91df46b8532b6c0a09a128c6669594f70bf
Dan Hogue <chmod740@gmail.com>
2011-06-22 16:16:23 -0600

    Pep 8 on imports

--- bzrflag/__init__.py
+++ bzrflag/__init__.py
@@ -1,12 +1,15 @@
-'''
-BZRFlag: BZFlag with Robots!
-'''
-import logging,os
-LOG_FILENAME = os.path.abspath(os.path.join(
-        os.path.split(__file__)[0], '..', 'debug.log'))
+"""BZRFlag: BZFlag with Robots!
 
-#from main import run
+"""
+import logging
+import os
+
 import config
+#from main import run
+
+LOG_FILENAME = os.path.abspath(os.path.join(
+    os.path.split(__file__)[0], '..', 'debug.log'))
+
 
 def run():
     config.init()
