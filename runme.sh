9d6fc25eaa8b8a82fd25d783362c58c28ff5d05a
Dan Hogue <chmod740@gmail.com>
2011-07-07 13:50:44 -0600

    Updated paths after rename

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 ./bin/bzrflag --world=maps/test.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
-python agents/ender.py localhost 50100 &
-python agents/agent0.py localhost 50101 &
-python agents/agent0.py localhost 50102 &
-python agents/agent0.py localhost 50103 &
+python bzagents/ender.py localhost 50100 &
+python bzagents/agent0.py localhost 50101 &
+python bzagents/agent0.py localhost 50102 &
+python bzagents/agent0.py localhost 50103 &
 
