8de32065443ce07bf57cc77c3b91acfe7ef1a425
Dan Hogue <chmod740@gmail.com>
2011-07-11 11:25:03 -0600

    Undid the last change.... turns out the classes need to be orderd as they are...

--- bzrflag/graphics.py
+++ bzrflag/graphics.py
@@ -43,244 +43,7 @@ from world import Base, Box
 from game import Tank, Shot, Flag, Base, Score
 
 DEFAULT_SIZE = map(int, config['window_size'].split('x'))
-
-
-class Display(object):
-    """Manages all graphics."""
-    _imagecache = ImageCache
-    _textclass = TextSprite
-    _spriteclass = BZSprite
-    _taunt = Taunt
-    
-    def __init__(self, game, screen_size=DEFAULT_SIZE):
-        self.game = game
-        self.world = config.world
-        self.scores = Scores()
-        self.taunt = self._taunt(self.game.map)
-        self.screen_size = screen_size
-        self.images = self._imagecache()
-        self._background = None
-        self.spritemap = {}
-        self.scale = 1
-        self.pos = [0,0]
-
-    def setup(self):
-        """Initializes pygame and creates the screen surface."""
-        pygame.init()
-        pygame.key.set_repeat(200,50)
-        self.setup_screen()
-        self.sprites = pygame.sprite.LayeredUpdates()
-        if config['python_console']:
-            cons = pygameconsole.PyConsole
-        else:
-            cons = pygameconsole.TelnetConsole
-        self.console = cons(self.game, (25,self.screen_size[1]*2/3-25,
-                            self.screen_size[0]-50,self.screen_size[1]/3))
-                            
-    def setup_screen(self):
-        """Sets up screen display."""
-        size = self.screen_size
-        self.screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-        self._screen = pygame.Surface(self.screen_size)
-        self._background = None
-        bg = self.background()
-        self.screen.blit(bg, (0, 0))
-        pygame.display.update()
-        
-    def resize(self, w, h):
-        """Resize the pygame surface."""
-        self.screen_size = w, h
-        self.setup_screen()
-        for sprite in self.sprites:
-            sprite.update(True)
-        self.redraw()
-
-    def rescale(self, scale, pos):
-        """Rescales display."""
-        if scale < 1:
-            return False
-        if scale > 20:
-            return False
-        oscale = self.scale
-        self.scale = scale
-
-        realpos = (pos[0] - self.pos[0])/oscale, (pos[1] - self.pos[1])/oscale
-        self.pos[0] = pos[0] - realpos[0]*self.scale
-        self.pos[1] = pos[1] - realpos[1]*self.scale
-        
-    def redraw(self):
-        """Redraws display."""
-        size = self._normal_background.get_rect().size
-        if self.pos[0]>0:self.pos[0] = 0
-        if self.pos[1]>0:self.pos[1] = 0
-        if self.pos[0]<self.screen_size[0] - size[0]*self.scale:
-            self.pos[0] = self.screen_size[0] - size[0]*self.scale
-        if self.pos[1]<self.screen_size[1] - size[1]*self.scale:
-            self.pos[1] = self.screen_size[1] - size[1]*self.scale
-        ## problem: jerky background.
-        tmp = pygame.Surface((size[0]/self.scale,size[1]/self.scale))
-        tmp.blit(self._normal_background, (self.pos[0]/self.scale-1, 
-                                           self.pos[1]/self.scale-1))
-        self._background = pygame.transform.smoothscale(tmp, size)
-        self.screen.blit(self._background,(0,0))
-        self.sprites.update()
-        for layer in self.sprites.layers():
-            for sprite in self.sprites.get_sprites_from_layer(layer):
-                self.screen.blit(sprite.image,sprite.rect)
-        self.console.draw(self.screen)
-        self.taunt.draw(self.screen)
-
-    def background(self):
-        """Creates a surface of the background with all obstacles.
-
-        Obstacles includes both bases and boxes.
-        
-        """
-        if not self._background:
-            wscale = self.world_to_screen_scale()
-            bg = self.images.tile(self.images.ground(), self.screen_size)
-            for box in self.game.map.obstacles:
-                s = TiledBZSprite(box, self.images.wall(), self)
-                bg.blit(s.image, s.rect.topleft)
-            self._normal_background = self._background = bg
-        return self._background
-
-    def update(self):
-        """Updates the state of all sprites and redraws the screen."""
-        bg = self.background()
-        self.sprites.clear(self.screen, bg)
-        self.sprites.update()
-        changes = self.sprites.draw(self.screen)
-        ## add a check for pygame input later
-        self.process_events()
-        self.scores.draw(self.screen)
-        self.console.draw(self.screen)
-        self.taunt.update()
-        self.taunt.draw(self.screen)
-        pygame.display.flip()
-        
-    def process_events(self):
-        dirty = False
-        for e in pygame.event.get():
-            self.console.event(e)
-            if e.type == QUIT:
-                self.game.running = False
-                self.game.map.end_game = True
-            elif e.type == MOUSEBUTTONDOWN:
-                if e.button == 4:
-                    dirty = True
-                    self.rescale(self.scale*1.1, e.pos)
-                elif e.button == 5:
-                    self.rescale(self.scale*(1/1.1), e.pos)
-                    dirty = True
-            elif e.type == KEYDOWN:
-                amt = 20
-                if not self.console.minimized:
-                    continue
-                if e.key == K_DOWN:
-                    self.pos[1] -= amt
-                elif e.key == K_UP:
-                    self.pos[1] += amt
-                elif e.key == K_LEFT:
-                    self.pos[0] += amt
-                elif e.key == K_RIGHT:
-                    self.pos[0] -= amt
-                else:
-                    continue
-                dirty = True
-            elif e.type == MOUSEMOTION:
-                if e.buttons[0]:
-                    self.pos[0]+=e.rel[0]
-                    self.pos[1]+=e.rel[1]
-                    dirty = True
-            elif e.type == VIDEORESIZE:
-                w,h = e.size
-                x = min(w, h)
-                self.resize(x, x)
-        if dirty:
-            self.redraw()
-
-    def pos_world_to_screen(self, pos):
-        """Converts a position from world space to screen pixel space.
-
-        >>> pos_world_to_screen((0, 0), (800, 800), (400, 400))
-        (200, 200)
-        >>> pos_world_to_screen((-400, -400), (800, 800), (400, 400))
-        (0, 400)
-        >>>
-        
-        """
-        x, y = pos
-        world_width, world_height = self.world.size
-        x += world_width / 2
-        y -= world_height / 2
-        x, y = self.vec_world_to_screen((x, y))
-        return x+self.pos[0], y+self.pos[1]
-
-    def size_world_to_screen(self, size):
-        """Converts sizes from BZFlag world space to screen space.
-
-        Note that bzflag sizes are more like a radius (half of width), so we
-        double them to normalize.
-        
-        """
-        w, h = size
-        w *= 2
-        h *= -2
-        screen_size = self.vec_world_to_screen((w, h))
-        return screen_size
-
-    def vec_world_to_screen(self, vector):
-        """Converts a vector from world space to screen pixel space.
-
-        >>> vec_world_to_screen((200, 200), (800, 800), (400, 400))
-        (100, -100)
-        >>>
-        """
-        wscale,hscale = self.world_to_screen_scale()
-        x, y = vector
-        return int(round(x * wscale)), -int(round(y * hscale))
-
-    def world_to_screen_scale(self):
-        screen_width, screen_height = self.screen_size
-        world_width, world_height = self.world.size
-        wscale = screen_width / float(world_width) * self.scale
-        hscale = screen_height / float(world_height) * self.scale
-        return wscale, hscale
-
-    def add_object(self, obj):
-        """Addes given object to display."""
-        types = (Tank, 'tank'), (Shot,'shot'), (Flag,'flag'), \
-                (Base,'base'), (Score,'score')
-        otype = None
-        for cls,name in types:
-            if isinstance(obj,cls):
-                otype = name
-                break
-        else:
-            raise Exception('invalid object added to display: %s'%obj)
-        if otype == 'score':
-            sprite = self._textclass(obj, self)
-            self.scores.add(sprite)
-        else:
-            image = self.images.loadteam(otype, obj.team.color)
-            sprite = self._spriteclass(obj, image, self, otype)
-            self.add_sprite(sprite, otype)
-            self.spritemap[obj] = sprite
-
-    def remove_object(self, obj):
-        self.remove_sprite(self.spritemap[obj])
-
-    def add_sprite(self,sprite,otype):
-        self.sprites.add(sprite,layer = ['base','tank',
-                                         'flag','shot','score'].index(otype))
-
-    def remove_sprite(self, sprite):
-        self.sprites.remove(sprite)
-
-    def kill(self):
-        pygame.display.quit()
-        
+      
         
 class ImageCache(object):
 
@@ -561,5 +324,242 @@ class Taunt(object):
             w, h = screen.get_rect().size
             mw, mh = self.img.get_rect().size
             screen.blit(self.img, (w/2-mw/2, h/2-mh/2))      
+   
             
-            
+class Display(object):
+    """Manages all graphics."""
+    _imagecache = ImageCache
+    _textclass = TextSprite
+    _spriteclass = BZSprite
+    _taunt = Taunt
+    
+    def __init__(self, game, screen_size=DEFAULT_SIZE):
+        self.game = game
+        self.world = config.world
+        self.scores = Scores()
+        self.taunt = self._taunt(self.game.map)
+        self.screen_size = screen_size
+        self.images = self._imagecache()
+        self._background = None
+        self.spritemap = {}
+        self.scale = 1
+        self.pos = [0,0]
+
+    def setup(self):
+        """Initializes pygame and creates the screen surface."""
+        pygame.init()
+        pygame.key.set_repeat(200,50)
+        self.setup_screen()
+        self.sprites = pygame.sprite.LayeredUpdates()
+        if config['python_console']:
+            cons = pygameconsole.PyConsole
+        else:
+            cons = pygameconsole.TelnetConsole
+        self.console = cons(self.game, (25,self.screen_size[1]*2/3-25,
+                            self.screen_size[0]-50,self.screen_size[1]/3))
+                            
+    def setup_screen(self):
+        """Sets up screen display."""
+        size = self.screen_size
+        self.screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+        self._screen = pygame.Surface(self.screen_size)
+        self._background = None
+        bg = self.background()
+        self.screen.blit(bg, (0, 0))
+        pygame.display.update()
+        
+    def resize(self, w, h):
+        """Resize the pygame surface."""
+        self.screen_size = w, h
+        self.setup_screen()
+        for sprite in self.sprites:
+            sprite.update(True)
+        self.redraw()
+
+    def rescale(self, scale, pos):
+        """Rescales display."""
+        if scale < 1:
+            return False
+        if scale > 20:
+            return False
+        oscale = self.scale
+        self.scale = scale
+
+        realpos = (pos[0] - self.pos[0])/oscale, (pos[1] - self.pos[1])/oscale
+        self.pos[0] = pos[0] - realpos[0]*self.scale
+        self.pos[1] = pos[1] - realpos[1]*self.scale
+        
+    def redraw(self):
+        """Redraws display."""
+        size = self._normal_background.get_rect().size
+        if self.pos[0]>0:self.pos[0] = 0
+        if self.pos[1]>0:self.pos[1] = 0
+        if self.pos[0]<self.screen_size[0] - size[0]*self.scale:
+            self.pos[0] = self.screen_size[0] - size[0]*self.scale
+        if self.pos[1]<self.screen_size[1] - size[1]*self.scale:
+            self.pos[1] = self.screen_size[1] - size[1]*self.scale
+        ## problem: jerky background.
+        tmp = pygame.Surface((size[0]/self.scale,size[1]/self.scale))
+        tmp.blit(self._normal_background, (self.pos[0]/self.scale-1, 
+                                           self.pos[1]/self.scale-1))
+        self._background = pygame.transform.smoothscale(tmp, size)
+        self.screen.blit(self._background,(0,0))
+        self.sprites.update()
+        for layer in self.sprites.layers():
+            for sprite in self.sprites.get_sprites_from_layer(layer):
+                self.screen.blit(sprite.image,sprite.rect)
+        self.console.draw(self.screen)
+        self.taunt.draw(self.screen)
+
+    def background(self):
+        """Creates a surface of the background with all obstacles.
+
+        Obstacles includes both bases and boxes.
+        
+        """
+        if not self._background:
+            wscale = self.world_to_screen_scale()
+            bg = self.images.tile(self.images.ground(), self.screen_size)
+            for box in self.game.map.obstacles:
+                s = TiledBZSprite(box, self.images.wall(), self)
+                bg.blit(s.image, s.rect.topleft)
+            self._normal_background = self._background = bg
+        return self._background
+
+    def update(self):
+        """Updates the state of all sprites and redraws the screen."""
+        bg = self.background()
+        self.sprites.clear(self.screen, bg)
+        self.sprites.update()
+        changes = self.sprites.draw(self.screen)
+        ## add a check for pygame input later
+        self.process_events()
+        self.scores.draw(self.screen)
+        self.console.draw(self.screen)
+        self.taunt.update()
+        self.taunt.draw(self.screen)
+        pygame.display.flip()
+        
+    def process_events(self):
+        dirty = False
+        for e in pygame.event.get():
+            self.console.event(e)
+            if e.type == QUIT:
+                self.game.running = False
+                self.game.map.end_game = True
+            elif e.type == MOUSEBUTTONDOWN:
+                if e.button == 4:
+                    dirty = True
+                    self.rescale(self.scale*1.1, e.pos)
+                elif e.button == 5:
+                    self.rescale(self.scale*(1/1.1), e.pos)
+                    dirty = True
+            elif e.type == KEYDOWN:
+                amt = 20
+                if not self.console.minimized:
+                    continue
+                if e.key == K_DOWN:
+                    self.pos[1] -= amt
+                elif e.key == K_UP:
+                    self.pos[1] += amt
+                elif e.key == K_LEFT:
+                    self.pos[0] += amt
+                elif e.key == K_RIGHT:
+                    self.pos[0] -= amt
+                else:
+                    continue
+                dirty = True
+            elif e.type == MOUSEMOTION:
+                if e.buttons[0]:
+                    self.pos[0]+=e.rel[0]
+                    self.pos[1]+=e.rel[1]
+                    dirty = True
+            elif e.type == VIDEORESIZE:
+                w,h = e.size
+                x = min(w, h)
+                self.resize(x, x)
+        if dirty:
+            self.redraw()
+
+    def pos_world_to_screen(self, pos):
+        """Converts a position from world space to screen pixel space.
+
+        >>> pos_world_to_screen((0, 0), (800, 800), (400, 400))
+        (200, 200)
+        >>> pos_world_to_screen((-400, -400), (800, 800), (400, 400))
+        (0, 400)
+        >>>
+        
+        """
+        x, y = pos
+        world_width, world_height = self.world.size
+        x += world_width / 2
+        y -= world_height / 2
+        x, y = self.vec_world_to_screen((x, y))
+        return x+self.pos[0], y+self.pos[1]
+
+    def size_world_to_screen(self, size):
+        """Converts sizes from BZFlag world space to screen space.
+
+        Note that bzflag sizes are more like a radius (half of width), so we
+        double them to normalize.
+        
+        """
+        w, h = size
+        w *= 2
+        h *= -2
+        screen_size = self.vec_world_to_screen((w, h))
+        return screen_size
+
+    def vec_world_to_screen(self, vector):
+        """Converts a vector from world space to screen pixel space.
+
+        >>> vec_world_to_screen((200, 200), (800, 800), (400, 400))
+        (100, -100)
+        >>>
+        """
+        wscale,hscale = self.world_to_screen_scale()
+        x, y = vector
+        return int(round(x * wscale)), -int(round(y * hscale))
+
+    def world_to_screen_scale(self):
+        screen_width, screen_height = self.screen_size
+        world_width, world_height = self.world.size
+        wscale = screen_width / float(world_width) * self.scale
+        hscale = screen_height / float(world_height) * self.scale
+        return wscale, hscale
+
+    def add_object(self, obj):
+        """Addes given object to display."""
+        types = (Tank, 'tank'), (Shot,'shot'), (Flag,'flag'), \
+                (Base,'base'), (Score,'score')
+        otype = None
+        for cls,name in types:
+            if isinstance(obj,cls):
+                otype = name
+                break
+        else:
+            raise Exception('invalid object added to display: %s'%obj)
+        if otype == 'score':
+            sprite = self._textclass(obj, self)
+            self.scores.add(sprite)
+        else:
+            image = self.images.loadteam(otype, obj.team.color)
+            sprite = self._spriteclass(obj, image, self, otype)
+            self.add_sprite(sprite, otype)
+            self.spritemap[obj] = sprite
+
+    def remove_object(self, obj):
+        self.remove_sprite(self.spritemap[obj])
+
+    def add_sprite(self,sprite,otype):
+        self.sprites.add(sprite,layer = ['base','tank',
+                                         'flag','shot','score'].index(otype))
+
+    def remove_sprite(self, sprite):
+        self.sprites.remove(sprite)
+
+    def kill(self):
+        pygame.display.quit()
+        
+                    
