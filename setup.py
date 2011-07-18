da2ad689163619bb23f9725d12cb504d36c554bc
Dan Hogue <chmod740@gmail.com>
2011-07-18 13:40:35 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BZRFlag',
+      version='1.0',
+      description='BZRFlag: BZFlag with Robots!',
+      author='BYU AML Lab',
+      author_email='kseppi@byu.edu',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      #packages=['distutils', 'distutils.command'],
+     )
