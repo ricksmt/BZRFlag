35d503624a04c015cd5014b524e371f477586644
Dan Hogue <chmod740@gmail.com>
2011-07-20 15:23:51 -0600

    Changed rungame.sh to use agent0 for all teams

--- rungame.sh
+++ rungame.sh
@@ -1,6 +1,6 @@
-./bin/bzrflag --world=maps/four_ls.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
+./bin/bzrflag --world=maps/test.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
-python bzagents/ender.py localhost 50100 &
+python bzagents/agent0.py localhost 50100 &
 python bzagents/agent0.py localhost 50101 &
 python bzagents/agent0.py localhost 50102 &
 python bzagents/agent0.py localhost 50103 &
