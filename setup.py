4542b57d21adbc726366a4913746069afde54374
Dan Hogue <chmod740@gmail.com>
2011-07-28 13:05:28 -0600

    Added copyright and license header to test files and deleated tests/test.bzw (test.bzw now lives in maps folder). Also removed TODO (moved to google code page)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='BZRFlag',
-      version='1.1',
+      version='1.0',
       description='BZRFlag: BZFlag with Robotic Tanks!',
       author='BYU AML Lab',
       author_email='kseppi@byu.edu',
--- tests/config.py
+++ tests/config.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python
+# Bzrflag
+# Copyright 2008-2011 Brigham Young University
+#
+# This file is part of Bzrflag.
+#
+# Bzrflag is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Bzrflag is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Bzrflag.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Inquiries regarding any further use of Bzrflag, please contact the Copyright
+# Licensing Office, Brigham Young University, 3760 HBLL, Provo, UT 84602,
+# (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
+
+"""Unit test for BZRFlag module config.py."""
+
+__author__ = "BYU AML Lab <kseppi@byu.edu>"
+__copyright__ = "Copyright 2008-2011 Brigham Young University"
+__license__ = "GNU GPL"
+
 import os
 
 import unittest
@@ -8,13 +36,14 @@ from bzrflag import config
 class ConfigTest(unittest.TestCase):
 
     def setUp(self):
-        self.world = ""+os.path.join(os.path.dirname(__file__), "test.bzw")
+        path = os.path.dirname(__file__)
+        self.world = os.path.join(path, "..", "maps", "test.bzw")
         self.port = "50100"
         self.config_file = config.Config(["--world="+self.world,
                                           "--red-port="+self.port])
 
     def tearDown(self):
-        self.config_file = None
+        del self.config_file
 
     def testArgError(self):
         args = '--world=test_bad.bzw --red-port=50189'.split()
